62302e21341e05c6636806d6d38e2e0cfc8a750a
Tulsi9 <72484847+Tulsi9@users.noreply.github.com>
2021-05-23 21:50:17 +0530

    Create my_twitter_bot.py

---
+++ my_twitter_bot.py
@@ -0,0 +1,53 @@
+import tweepy
+import time
+
+print('this is my twitter bot', flush=True)
+
+CONSUMER_KEY='a0S4NrD9POOTOdWP4xudzqLQT'
+CONSUMER_SECRET='3O999RMzwedyESCTtdaMVUNrfsHtf7XToMv4VULJm37Q7HhY5o'
+ACCESS_KEY='1396363539019800579-436JAIiR23kg8UB8HXcNPJ2fnRbo89'
+ACCESS_SECRET='Hvpl1sfUUC4422l14xJNAc1EVqd6S8yeyWRgMrNq5J8oq'
+
+print('this is my twitter bot', flush=True)
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+FILE_NAME = 'last_seen_id.txt'
+
+def retrieve_last_seen_id(file_name):
+    f_read = open(file_name, 'r')
+    last_seen_id = int(f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+def store_last_seen_id(last_seen_id, file_name):
+    f_write = open(file_name, 'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
+
+def reply_to_tweets():
+    print('retrieving and replying to tweets...', flush=True)
+    # DEV NOTE: use 1060651988453654528 for testing.
+    last_seen_id = retrieve_last_seen_id(FILE_NAME)
+    # NOTE: We need to use tweet_mode='extended' below to show
+    # all full tweets (with full_text). Without it, long tweets
+    # would be cut off.
+    mentions = api.mentions_timeline(
+                        last_seen_id,
+                        tweet_mode='extended')
+    for mention in reversed(mentions):
+        print(str(mention.id) + ' - ' + mention.full_text, flush=True)
+        last_seen_id = mention.id
+        store_last_seen_id(last_seen_id, FILE_NAME)
+        if '#helloworld' in mention.full_text.lower():
+            print('found #helloworld!', flush=True)
+            print('responding back...', flush=True)
+            api.update_status('@' + mention.user.screen_name +
+                    '#HelloWorld back to you!', mention.id)
+
+while True:
+    reply_to_tweets()
+    time.sleep(15)
